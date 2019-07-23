dfe92f287d9b64125c6aa6f3557f474c10790e6d
dokasov <camilogozp@gmail.com>
2019-07-23 01:03:37 -0500

    Nueva versión

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log('Server started on port '+port))
