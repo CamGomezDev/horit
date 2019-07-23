61e8ad6c2b85eaa9f39ecab6a8efad9252547a34
dokasov <camilogozp@gmail.com>
2019-07-23 01:02:06 -0500

    Nueva versión

---
+++ client/build/precache-manifest.10bf4c19e130b92eeb86d2311303067e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa792a73949a4b4c89350faf0317c40a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dbc89efea7f78bfbc93",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "799bc63a740d44713f8b",
+    "url": "/static/css/main.f064a45b.chunk.css"
+  },
+  {
+    "revision": "0dbc89efea7f78bfbc93",
+    "url": "/static/js/2.ba3a9e14.chunk.js"
+  },
+  {
+    "revision": "799bc63a740d44713f8b",
+    "url": "/static/js/main.3bda4e01.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
